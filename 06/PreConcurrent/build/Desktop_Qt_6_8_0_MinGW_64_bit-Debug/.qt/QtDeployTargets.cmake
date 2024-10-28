41488ea54e16d97468087d49c6ce0e313717ebdc
ilyapatis24 <ilyapatis24@mail.ru>
2024-10-28 23:45:43 +0300

    remove build results from repo

--- 06/PreConcurrent/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Concurrent_FILE D:/qt-homeworks/06/PreConcurrent/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/Concurrent.exe)
-set(__QT_DEPLOY_TARGET_Concurrent_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Concurrent_RUNTIME_DLLS D:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.8.0/mingw_64/bin/Qt6Concurrent.dll;D:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)

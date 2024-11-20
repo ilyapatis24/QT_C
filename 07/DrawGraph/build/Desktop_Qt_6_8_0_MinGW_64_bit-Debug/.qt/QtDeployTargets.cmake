c82ce6159f7c0a81207891e38ffc958a8979502c
ilyapatis24 <ilyapatis24@mail.ru>
2024-11-20 13:17:04 +0300

    remove build

--- 07/DrawGraph/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_DrawGraph_FILE D:/qt-homeworks/07/DrawGraph/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/DrawGraph.exe)
-set(__QT_DEPLOY_TARGET_DrawGraph_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_DrawGraph_RUNTIME_DLLS D:/Qt/6.8.0/mingw_64/bin/Qt6Concurrent.dll;D:/Qt/6.8.0/mingw_64/bin/Qt6Charts.dll;D:/Qt/6.8.0/mingw_64/bin/Qt6OpenGLWidgets.dll;D:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.8.0/mingw_64/bin/Qt6OpenGL.dll;D:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
